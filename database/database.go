d9ee3b80e60b38cfe319dc27c53952c87a65bfad
Abhinav <abhinav.mishra@gmail.com>
2022-01-11 08:25:31 +0530

    initial commit

---
+++ database/database.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/mysql"
+)
+
+var Db *sql.DB
+
+func InitDB() {
+	db, err := sql.Open("mysql", "abhinav:Ab9450455997@tcp(192.168.1.2:3306)/gql1?multiStatements=true")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Panic(err)
+	}
+	Db = db
+}
+
+func Migrate() {
+	if err := Db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	driver, _ := mysql.WithInstance(Db, &mysql.Config{})
+	url := fmt.Sprintf("file://migrations")
+	fmt.Println("url: ", url)
+	m, err := migrate.NewWithDatabaseInstance(
+		url,
+		"gql1",
+		driver,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		fmt.Println("#37", err)
+		log.Fatal(err)
+	}
+
+}
---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/abhinavmishra094/anothergraphqlattempt/database"
+	"github.com/abhinavmishra094/anothergraphqlattempt/graph"
+	"github.com/abhinavmishra094/anothergraphqlattempt/graph/generated"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	database.InitDB()
+	database.Migrate()
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
